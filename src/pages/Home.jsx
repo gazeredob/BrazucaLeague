7dfa6bc76b879b8fc17bdab31f49f0a0e8172fee
gazeredob <gabrielazeredob@gmail.com>
2025-07-08 22:19:39 -0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-blue-700 p-6 rounded-lg mb-6 shadow-lg">
+        <h2 className="text-3xl font-bold">Bem-vindo à Brazuca League</h2>
+        <p className="mt-2">Futebol com a ginga brasileira. De 23 a 26/09, prepare-se para um novo campeonato no estilo Kings League.</p>
+      </div>
+    </div>
+  );
+}
