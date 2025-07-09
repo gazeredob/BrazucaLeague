7b4f02943606fc4aef88dca01d30fff54904c111
gazeredob <gabrielazeredob@gmail.com>
2025-07-08 22:20:20 -0300

    Create Votacao.jsx

---
+++ src/pages/Votacao.jsx
@@ -0,0 +1,9 @@
+export default function Votacao() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Vote no Melhor Jogador da Partida</h2>
+      <p className="mb-4">Partida fictícia: Estrelas do Sul x Fúria Gaúcha</p>
+      <button className="bg-yellow-500 text-black px-4 py-2 rounded hover:bg-yellow-400">Votar Agora</button>
+    </div>
+  );
+}
